786d5dd6f3405c9419f54d8672405a4d0d4e6af5
rosebudroro <roxy.osorio@moo.com>
2025-06-04 15:50:23 -0400

    like model

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -2,9 +2,29 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
+use MongoDB\Laravel\Eloquent\Model;
+// use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
 
 class Comment extends Model
 {
     //
+    protected $connection = 'mongodb';
+    protected $table = 'comments';
+    protected $fillable = [
+        'content',
+        'user_id',
+        'post_id',
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(related: User::class);
+    }
+
+    public function post (): BelongsTo
+    {
+        return $this->belongsTo(related: Post::class);
+    }
+
 }
