f802853fd13304ef777f1a35b5377491945ac041
rosebudroro <roxy.osorio@moo.com>
2025-06-04 14:12:13 -0400

    setting up

---
+++ app/Models/Post.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+
+use MongoDB\Laravel\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Post extends Model
+{
+    //
+    protected $connection = 'mongodb';
+    protected $table = 'posts';
+    protected $fillable = [
+        'caption',
+        'image_path',
+        'image_url',
+        'user_id',
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(related: User::class);
+    }
+
+    public function comments(): HasMany
+    {
+        return $this->hasMany(related: Comment::class);
+    }
+
+    public function likes(): HasMany
+    {
+        return $this->hasMany(related: Like::class);
+    }
+}
--- app/Models/User.php
+++ app/Models/User.php
@@ -3,14 +3,17 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
+use MongoDB\Laravel\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
+    use Notifiable;
+
+    protected $connection = 'mongodb';
+    protected $table = 'users';
 
     /**
      * The attributes that are mass assignable.
@@ -19,30 +22,36 @@ class User extends Authenticatable
      */
     protected $fillable = [
         'name',
+        'username',
         'email',
         'password',
+        'profile_image',
+        'bio'
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var list<string>
-     */
     protected $hidden = [
         'password',
         'remember_token',
     ];
 
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
-    protected function casts(): array
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+        'password' => 'hashed',
+    ];
+
+    public function posts(): HasMany
     {
-        return [
-            'email_verified_at' => 'datetime',
-            'password' => 'hashed',
-        ];
+        return $this->hasMany(related: Post::class);
     }
+
+    public function likes(): HasMany
+    {
+        return $this->hasMany(related: Like::class);
+    }
+
+    public function comments(): HasMany
+    {
+        return $this->hasMany(related: Comment::class);
+    }
+
 }
